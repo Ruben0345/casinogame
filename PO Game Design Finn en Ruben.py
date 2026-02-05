4dfc0419b321ca8084356b2c52d53a1f88f3d2e3
Ruben0345 <224572@lekenlinge.nl>
2026-02-05 13:58:20 +0100

    platform

--- PO Game Design Finn en Ruben.py
+++ PO Game Design Finn en Ruben.py
@@ -1,1 +1,14 @@
 import pygame as p
+
+screen = p.display.set_mode((800, 600))
+p.display.set_caption("casino basis")
+
+# Load image
+platform = p.image.load ("Tokyo Ghoul.png")
+
+# Main loop
+running = True
+while running:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            running = False
