905b0ddb412605175306cbf75753abdbd63f9644
Ruben0345 <224572@lekenlinge.nl>
2026-02-05 15:44:33 +0100

    platform werkend

--- PO Game Design Finn en Ruben.py
+++ PO Game Design Finn en Ruben.py
@@ -8,7 +8,13 @@ platform = p.image.load ("Tokyo Ghoul.png")
 
 # Main loop
 running = True
-while running:
+clock = p.time.Clock()
+while running == True:
     for event in p.event.get():
         if event.type == p.QUIT:
             running = False
+
+    screen.blit(platform, (0, 0))
+    p.display.flip()
+    clock.tick(60)
+p.quit()
